1988b6646830d07705e952544f59e2e743268a6a
RoyMolina03 <140010399+RoyMolina03@users.noreply.github.com>
2024-05-17 23:45:32 -0400

    Organized Classes, Introduced New Classes

---
+++ deckbase/src/Card/Card.java
@@ -0,0 +1,11 @@
+package Card;
+
+public class Card {
+    //private members
+    private String name;
+    private String color;
+    private String set;
+    private int cost;
+
+
+}
---
+++ deckbase/src/Util/Printable.java
@@ -0,0 +1,5 @@
+package Util;
+
+public class Printable {
+    //this class is going to be reading and writing from the .txt file for memory
+}
