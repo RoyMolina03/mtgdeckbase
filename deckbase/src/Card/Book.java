01ea690b7ed31799ac9c6ec180e026add3437022
RoyMolina03 <140010399+RoyMolina03@users.noreply.github.com>
2024-09-09 16:57:33 -0400

    Updated Book.java
    added constructors

--- deckbase/src/Card/Book.java
+++ deckbase/src/Card/Book.java
@@ -1,5 +1,19 @@
 package Card;
 
+import Util.*;
+
 public class Book {
+
+    DoublyLinkedList Collection = new DoublyLinkedList();
+
+    //initialize user collection with one card already
+    public Book (Card data)
+    {
+        System.out.println("Creating Collection, adding Card");
+        Collection.insertAtFront(data);
+    }
+
+    
+
 
 }
